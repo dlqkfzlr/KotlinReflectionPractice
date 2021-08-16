6d5c9e9972f80821a404bf8fe7fad606018ebc34
sw.choi <sw.choi@toss.im>
2021-08-17 01:09:43 +0900

    viewBinding 적용

--- app/src/main/java/com/example/kotlinreflectionpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinreflectionpractice/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.kotlinreflectionpractice
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.kotlinreflectionpractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
 }
