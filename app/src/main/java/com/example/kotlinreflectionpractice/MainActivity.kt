b9a126670357092651ede274a5bbc744ec25583d
sw.choi <sw.choi@toss.im>
2021-08-28 21:22:34 +0900

    PermissionController.kt 생성 및 적용

--- app/src/main/java/com/example/kotlinreflectionpractice/MainActivity.kt
+++ app/src/main/java/com/example/kotlinreflectionpractice/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.example.kotlinreflectionpractice
 
+import android.Manifest
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.lifecycleScope
+import com.example.kotlinreflectionpractice.PermissionResult.*
 import com.example.kotlinreflectionpractice.databinding.ActivityMainBinding
+import com.example.kotlinreflectionpractice.permission.PermissionController
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+            private const val REQUEST_ID = 0x0001
+    }
 
     private lateinit var binding: ActivityMainBinding
 
@@ -17,6 +29,35 @@ class MainActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        lifecycleScope.launchWhenResumed {
+           Log.d("PERMISSION", "start requestPermissions")
+            val permissionResult = PermissionController.requestPermissions(
+                this@MainActivity,
+                REQUEST_ID,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.READ_CONTACTS,
+                Manifest.permission.CAMERA,
+            )
+            Log.d("PERMISSION_RESULT", "$permissionResult")
+            when(permissionResult) {
+                is PermissionGranted -> {
+
+                }
+                is PermissionDenied -> {
+
+                }
+                is PermissionDeniedPermanently -> {
+                    // 실제론 Dialog 띄운 후, 설정화면으로 이동시켜줘야함
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, "권한없어서 종료", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
+                is ShowRationale -> {
+
+                }
+            }
+        }
     }
 
 }
--- app/src/main/java/com/example/kotlinreflectionpractice/permission/PermissionController.kt
+++ app/src/main/java/com/example/kotlinreflectionpractice/permission/PermissionController.kt
@@ -1,6 +1,8 @@
 package com.example.kotlinreflectionpractice.permission
 
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.kotlinreflectionpractice.PermissionResult
 import com.example.kotlinreflectionpractice.permission.base.BasePermissionController
 import kotlinx.coroutines.CompletableDeferred
@@ -10,8 +12,9 @@ import kotlinx.coroutines.withContext
 class PermissionController: BasePermissionController() {
 
     companion object {
-        /*
-        *//** Invoke from Activity *//*
+
+        private const val TAG = "PermissionManager"
+
         suspend fun requestPermissions(
             activity: AppCompatActivity,
             requestId: Int,
@@ -21,30 +24,51 @@ class PermissionController: BasePermissionController() {
                 return@withContext _requestPermissions(
                     activity,
                     requestId,
-                    *permissions
+                    *permissions    // spread operator
                 )
             }
         }
 
-        *//** Invoke from Fragment *//*
-        suspend fun requestPermissions(
-            fragment: androidx.fragment.app.Fragment,
+        private suspend fun _requestPermissions(
+            activityOrFragment: Any,
             requestId: Int,
             vararg permissions: String
         ): PermissionResult {
-            return withContext(Dispatchers.Main) {
-                return@withContext _requestPermissions(
-                    fragment,
+            val fragmentManager = if (activityOrFragment is AppCompatActivity) {
+                activityOrFragment.supportFragmentManager
+            } else {
+                (activityOrFragment as Fragment).childFragmentManager
+            }
+            return if (fragmentManager.findFragmentByTag(TAG) != null) {
+                val permissionController = fragmentManager.findFragmentByTag(TAG) as PermissionController
+                permissionController.completableDeferred = CompletableDeferred()
+                permissionController.requestPermissions(
                     requestId,
                     *permissions
                 )
+                permissionController.completableDeferred.await()
+            } else {
+                val permissionController = PermissionController().apply {
+                    completableDeferred = CompletableDeferred()
+                }
+                fragmentManager.beginTransaction().add(
+                    permissionController,
+                    TAG
+                ).commitNow()
+                permissionController.requestPermissions(requestId, *permissions)
+                permissionController.completableDeferred.await()
             }
-        }*/
+        }
+
     }
 
+    /*
+    * All functions on this interface [and all interfaces derived from it] are thread-safe
+    * and can be safely invoked from concurrent coroutines without external synchronization.” */
     private lateinit var completableDeferred: CompletableDeferred<PermissionResult>
 
     override fun onPermissionResult(permissionResult: PermissionResult) {
+        Log.d("PermissionController", "onPermissionResult(permissionResult=${permissionResult})")
         if (::completableDeferred.isInitialized) {
             completableDeferred.complete(permissionResult)
         }
